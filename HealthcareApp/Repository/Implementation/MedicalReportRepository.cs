5bfde6520dba521fae2f273592572a0a8c12453a
Lino2007 <lbevanda1@etf.unsa.ba>
2023-11-01 23:39:28 +0100

    Added base repository structure

---
+++ HealthcareApp/Repository/Implementation/MedicalReportRepository.cs
@@ -0,0 +1,12 @@
+﻿using HealthcareApp.Models.DataModels;
+using HealthcareApp.Repository.Interface;
+
+namespace HealthcareApp.Repository.Implementation
+{
+    public class MedicalReportRepository : CrudRepository<MedicalReport>, IMedicalReportRepository
+    {
+        public MedicalReportRepository(HealthcareDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ HealthcareApp/Repository/Implementation/PatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using HealthcareApp.Models.DataModels;
+using HealthcareApp.Repository.Interface;
+
+namespace HealthcareApp.Repository.Implementation
+{
+    public class PatientRepository : CrudRepository<Patient>, IPatientRepository
+    {
+        public PatientRepository(HealthcareDbContext context) : base(context)
+        {
+        }
+    }
+}
