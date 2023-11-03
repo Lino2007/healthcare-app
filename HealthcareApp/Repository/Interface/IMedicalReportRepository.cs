57a3a150cd25fbb44596f205c8a728d5ea1fce68
Lino Bevanda <44145624+Lino2007@users.noreply.github.com>
2023-11-03 22:25:31 +0100

    Connect Medical Report view with repository layer (#7)
    * Connect Medical Report view with repository layer 
    
    * Add custom repository layer methods for displaying detailed Medical Report

--- HealthcareApp/Repository/Interface/IMedicalReportRepository.cs
+++ HealthcareApp/Repository/Interface/IMedicalReportRepository.cs
@@ -4,5 +4,8 @@ namespace HealthcareApp.Repository.Interface
 {
     public interface IMedicalReportRepository : ICrudRepository<MedicalReport>
     {
+        public Task<List<MedicalReport>> GetAllDetailedMedicalReports();
+        public Task<MedicalReport?> GetDetailedMedicalReport(Guid id);
+
     }
 }
